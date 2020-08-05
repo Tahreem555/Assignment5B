6cca420385d8bb43ffa3211223d4fa32a339c77c
Tahreem555 <65005628+Tahreem555@users.noreply.github.com>
2020-08-05 23:30:02 +0500

    Assg5B

---
+++ Assets/Scripts/PlayerController11.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+public class PlayerController11 : MonoBehaviour
+{
+    public float speed;
+    public GameObject theSphere;
+
+    public Text CountText;
+
+    public Text winText;
+
+    private Rigidbody rb;
+    string revs = "";
+    private int count;
+    public AudioSource touchSound;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winText.text = "";
+        touchSound = GetComponent<AudioSource>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            string objectName = other.gameObject.name;
+            char c = objectName.Last();
+            string sphere = ("Sphere" + c);
+            string revs = "";
+            Text name = GameObject.Find(sphere + "/Canvas/Text").GetComponent<Text>();
+            string myst = name.text.ToString();
+            for (int i = myst.Length - 1; i >= 0; i--) //String Reverse  
+            {
+                revs += myst[i].ToString();
+            }
+            Debug.Log(revs.Equals(myst));
+            Debug.Log("revs  " + revs);
+            Debug.Log("myst  " + revs);
+
+            if ((string.Equals(myst, revs)) == true) // Checking whether string is palindrome or not  
+            {
+                theSphere = GameObject.Find(sphere);
+                other.gameObject.SetActive(false);
+                theSphere.SetActive(false);
+                count = count + 1;
+                touchSound.Play();
+            }
+            Debug.Log(count);
+        }
+        setCountText();
+
+    }
+    void setCountText()
+    {
+        CountText.text = count.ToString() + "  Count: ";
+        // /*CountText.text = "count:" + count.ToString();
+        if (count >= 3 && count <= 10)
+        {
+            winText.text = "The Number of captured Palindrome is:";
+        }
+
+    }
+}
---
+++ Assets/Scripts/SpawnObject.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnObject : MonoBehaviour
+{
+
+    string  catchvalue;
+    private GameObject particle;
+    public  GameObject pickCube;
+
+    public GameObject childobject;
+
+    findObjectChild objchild = new findObjectChild();
+
+
+    //public Vector3 center;
+    //public Vector3 size;
+    //public LayerMask mask;
+    //public bool checkCubefun = false;
+
+   // public GameObject CubePrefab;
+    private GameObject txt_box_parent;
+    public Text txt_box;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(cubespawn());
+    }
+
+    IEnumerator cubespawn()
+    {
+        for (int i = 1; i <= 25; i += 1)
+        {
+            
+            Instantiate(pickCube, new Vector3(Random.Range(5f, 48f), 1f, Random.Range(50f, 70f)), Quaternion.identity);
+            yield return new WaitForSeconds(0.006f);
+            if(i < 25)
+            {
+                childobject.GetComponent<TextMesh>().text = objchild.MatchingParanthesis();
+            }
+            else
+            {
+                childobject.GetComponent<TextMesh>().text = objchild.changeString();
+               
+                particle = childobject.gameObject.transform.Find("Particle System").gameObject;
+                particle.SetActive(false);
+            }
+            
+           
+          
+        }
+        
+        for (int i = 26; i <= 55; i += 1)
+        {
+            Instantiate(pickCube, new Vector3(Random.Range(28f, 61f), 1f, Random.Range(30f, 90f)), Quaternion.identity);
+            yield return new WaitForSeconds(0.005f);
+            
+        }
+
+        for (int i = 56; i <= 65; i += 1)
+        {
+            Instantiate(pickCube, new Vector3(Random.Range(71f, 77f), 1f, Random.Range(19f, 40f)), Quaternion.identity);
+            yield return new WaitForSeconds(0.005f);
+
+        }
+
+        for (int i = 66; i <= 83; i += 1)
+        {
+            Instantiate(pickCube, new Vector3(Random.Range(4f, 70f), 1f, Random.Range(1f, 20f)), Quaternion.identity);
+            yield return new WaitForSeconds(0.005f);
+
+        }
+        
+    }
+
+    public void setText()
+    {
+        Vector3 namePos = Camera.main.WorldToScreenPoint(this.transform.position);
+        txt_box.transform.position = namePos;
+    }
+
+
+}
